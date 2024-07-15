36ec8a809b72bf912e711ad9127bd9ad89ef7388
Ugo <ugochukwuobianuonye@gmail.com>
2024-07-15 17:07:10 +0100

    I've "re-perfected" my code again🥲 That extra line of code that i added feels too explicit, I'm not happy with it but if it works is works. Anyways yeah i hope I don't need to do anymore

--- Age-calculator-app/src/DateDisplay.jsx
+++ Age-calculator-app/src/DateDisplay.jsx
@@ -20,13 +20,20 @@ export default function DateDisplay({ date, hasSubmitted }) {
         //monthDiff = 7 - 12 = -5, so what we can do here is basically the same as what we do in the second if statement, we can find the distance from december to the end of the year, then find the distance from the start of the year to July
         // so therefore, we have 7 + (12 - 12) = 7
       }
-      if (dayDiff < 0) {
+      if (dayDiff < 0 && monthDiff > 0) {
         let birthMonthDays = new Date(date.year, date.month + 1, 0).getDate();
         dayDiff += birthMonthDays;
         monthDiff--;
         //so basically what I'm doing here is... If your current date is 12th of july and your date of birth is 25th of january for example,
         /// then dayDiff = 12 - 25 = -13days, so we then add the total number days in that month(january) to get the number of days from january 25 to the end of january and add it to the number of days from the beginning of July to July 12th
         //so therefore dayDiff = 12 + (31 - 25) = 18 days
+      }
+      if (dayDiff < 0 && monthDiff === 0) {
+        //same month, past year, future day
+        let birthMonthDays = new Date(date.year, date.month + 1, 0).getDate();
+        dayDiff += birthMonthDays;
+        monthDiff = 11;
+        yearDiff--;
       }
       setState({ dayDiff, monthDiff, yearDiff });
     }
